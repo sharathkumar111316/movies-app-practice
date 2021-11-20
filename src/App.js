1f7c2a128e25a28634dee925f1e3076e84dc0434
sharath <sharathkumar111316@gmail.com>
2021-11-21 03:32:03 +0530

    added https to the api url since it cannot access unsecured http

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
   await fetch(url)
     .then((response) => { return response.json(); })
